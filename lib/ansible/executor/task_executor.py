544174b6cd46334ee945f1858ca79613010f2f13
Patrick Kingston <pkingsto@redhat.com>
2025-10-03 17:11:04 -0400

    Remove unreachable code path
    ignore_errors comes from the playbook ignore_errors,
    and item_ignore comes from '_ansible_ignore_errors'
    which comes from line 330 which is where task_fields
    simply gets set to ignore_errors thus the test is
    (True if not True) or (False if not False)

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -120,8 +120,6 @@ class TaskExecutor:
                                 res['failed'] = True
                                 res['msg'] = 'One or more items failed'
                                 self._task.ignore_errors = item_ignore
-                            elif self._task.ignore_errors and not item_ignore:
-                                self._task.ignore_errors = item_ignore
                         if 'unreachable' in item and item['unreachable']:
                             item_ignore_unreachable = item.pop('_ansible_ignore_unreachable')
                             if not res.get('unreachable'):
