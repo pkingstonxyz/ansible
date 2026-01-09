6e5bb64ab3ec3e54d0b6d70416398823d77621cf
Patrick Kingston <pkingsto@redhat.com>
2026-01-09 15:01:09 -0500

    Clarify comment

--- lib/ansible/galaxy/collection/__init__.py
+++ lib/ansible/galaxy/collection/__init__.py
@@ -844,8 +844,10 @@ def verify_collections(
                         format(coll_type=collection.type)
                     )
 
-                # NOTE: Add the found collection as the first place to check
                 search_paths = (pathlib.Path(path) / collection.namespace / collection.name for path in search_paths)
+
+                # NOTE: This guarantees finding the collection if the
+                # NOTE: containing folder is improperly named
                 if installed_collection := installed_collections.get(collection.fqcn, None):
                     search_paths = chain([pathlib.Path(to_text(installed_collection.src))], search_paths)
 
